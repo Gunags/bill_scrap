7d83ad41dd0adb4c0bdc756a7d34778e41373ab8
gunasekaran <gunasekaran.sg10@gmail.com>
2023-01-05 13:48:07 +0530

    add loginpages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { UserLogin } from "./components/login/UserLogin";
 import { AdminLogin } from "./components/login/AdminLogin";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { Home } from "./components/Pages/Home";
 import bg from "./Image/2RNb.gif";
 function App() {
