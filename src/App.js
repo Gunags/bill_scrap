53b626de93dae4b6eec6dc662c282c497dbf9c27
gunasekaran <gunasekaran.sg10@gmail.com>
2023-01-05 14:31:38 +0530

    add loginpages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { UserLogin } from "./components/login/UserLogin";
 import { AdminLogin } from "./components/login/AdminLogin";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Home } from "./components/Pages/Home";
 import bg from "./Image/2RNb.gif";
-function App() {
+export const App = () => {
   const myStyle = {
     backgroundImage: `url(${bg})`,
     height: "100vh",
@@ -13,15 +13,13 @@ function App() {
   };
   return (
     <div className="App" style={myStyle}>
-      <Router basename="/bill_scrap">
+      <Router>
         <Routes>
-          <Route path="/bill_scrap" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/adminlogin" element={<AdminLogin />} />
           <Route path="/userlogin" element={<UserLogin />} />
         </Routes>
       </Router>
     </div>
   );
-}
-
-export default App;
+};
