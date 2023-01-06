a4b9a49047a17e75d720027560e19040cb700751
gunasekaran <gunasekaran.sg10@gmail.com>
2023-01-06 12:55:33 +0530

    add login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import React from "react";
-import { UserLogin } from "./components/login/UserLogin";
-import { AdminLogin } from "./components/login/AdminLogin";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Home } from "./components/Pages/Home";
-import bg from "./Image/2RNb.gif";
-export const App = () => {
+import { UserLogin } from './components/login/UserLogin';
+import { AdminLogin } from './components/login/AdminLogin';
+import { Route, Routes } from 'react-router-dom';
+import { Home } from './components/Pages/Home';
+import bg from './Image/2RNb.gif';
+import React from 'react';
+import { NavigationBar } from './components/Navbar/NavigationBar';
+
+const App = () => {
   const myStyle = {
     backgroundImage: `url(${bg})`,
-    height: "100vh",
-    backgroundSize: "cover",
-    backgroundRepeat: "no-repeat",
+    height: '100vh',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
   };
   return (
-    <div className="App" style={myStyle}>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/adminlogin" element={<AdminLogin />} />
-          <Route path="/userlogin" element={<UserLogin />} />
-        </Routes>
-      </Router>
+    <div style={myStyle}>
+      <NavigationBar />
+      <Routes>
+        <Route exact path='/' component={Home} />
+        <Route path='/adminlogin' component={AdminLogin} />
+      </Routes>
     </div>
   );
 };
+
+export default App;
+
+// export const App = () => {
+
+//   return;
+//   <div style={myStyle}>
+//     APP
+//     {/* <Router>
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/adminlogin" element={<AdminLogin />} />
+//         <Route path="/userlogin" element={<UserLogin />} />
+//       </Routes>
+//     </Router> */}
+//   </div>;
+// };
